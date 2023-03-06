cf4d12f292a7ec80aeb9314624f85eb6e7e0212d
jamila <elmousaouyjamila@gmail.com>
2023-03-06 07:29:23 -0800

    task

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,6 @@
-#include "main.h"                                                                /**                                                                                                                                     
+#include "main.h"         
+
+/**                                                                                                                                     
  * *_memcpy -  copies memory area from src to dest                                                                                      
  * @dest: destination                                                                                                                   
  * @src: source                                                                                                                         
@@ -9,11 +11,11 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {                                                                                                                                       
         unsigned int i;                                                                                                                 
                                                                                                                                         
-        for (i = 0;i < n; i++)                                                                                                   
+        for (i = 0; n > 0 ; i++,n--)                                                                                                   
         {                                                                                                                               
                 dest[i] = src[i]; 
 
-                  n--;		
+                  
         }                                                                                                                               
                                                                                                                                         
         return (dest);                                                                                                                  
