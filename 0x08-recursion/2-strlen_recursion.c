cd23b5581339b6072330d3bdd8da3660d159e5a5
jamila <elmousaouyjamila@gmail.com>
2023-03-09 06:18:43 -0800

    task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strlen - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+int _strlen_recursion(char *s)
+{
+	int n = 0;
+	
+
+	if (*s)
+	{
+
+		n++;
+		n= n+ _strlen_recursion(s+1);
+	}
+	return (n);
+}
